232dbb64be7b3346d2ab5ad29d0bf120a8f845c9
Sweper27 <rifqiariessaputra@gmail.com>
2026-03-24 12:24:04 +0700

    praktikum 2.4

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -4,18 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrCreate(
-            [
-                'username' => 'manager',
-                'nama' => 'Manager',
-            ],
-        );
-
-        return view('user', ['data' => $user]);
+        $user = UserModel::firstOrNew(
+        [
+            'username' => 'manager33',
+            'nama' => 'Manager Tiga Tiga',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
+        ],
+    );
+    $user->save();
+    return view('user', ['data' => $user]);
     }
 }
